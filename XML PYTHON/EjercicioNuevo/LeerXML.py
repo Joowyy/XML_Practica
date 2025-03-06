162ac2e533d1f1e44f9ae6d1860eeabc9dfc3b17
Joowyy <joelsanchezfernandez05@gmail.com>
2025-03-06 20:29:08 +0100

    Ejercicio corregido

--- XML PYTHON/EjercicioNuevo/LeerXML.py
+++ XML PYTHON/EjercicioNuevo/LeerXML.py
@@ -1,10 +1,10 @@
 from lxml import etree
 
 # Habilita la resolución de entidades externas
-varParser = etree.XMLParser(resolve_entities=True, load_dtd=True)
+varParser = etree.XMLParser(resolve_entities=True, load_dtd=True, no_network=False)
 
 # Carga el archivo XML
-xml_path = "Practica_Variables2.xml"
+xml_path = "Practica.xml"
 elParser = etree.parse(xml_path, varParser)
 
 # Mostrar el XML procesado
